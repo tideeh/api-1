8f3ca846b7778e7728aa10b94be2e5bbcc62307b
Andre Martins Pereira <andre.martins@engesoftware.com.br>
2022-07-07 13:12:50 -0300

    adicionado metodo para deletar todos os artigos

--- app/Http/Controllers/ArtigoController.php
+++ app/Http/Controllers/ArtigoController.php
@@ -104,7 +104,7 @@ class ArtigoController extends Controller
 
     public function destroyAll()
     {
-        // deleta todos os artigos
+        // deleta todos os artigos existentes
         foreach (Artigo::all() as $artigo) {
             $artigo->delete();
         }
